763b3d6b4ac812e7a4bafc4c72b18de16e58f380
Josh Adamous <joshadamous@gmail.com>
2023-04-05 21:49:33 -0500

    Updating url in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/external-fee-test/',
+  base: 'https://joshadamous.github.io/external-fee-test/',
   plugins: [react()],
   server: {
     host: true,
