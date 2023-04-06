1ba994394964e180f15096b6c30d8e8fad9ae619
Josh Adamous <joshadamous@gmail.com>
2023-04-05 21:46:38 -0500

    Updating url base ending slash

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/external-fee-test',
+  base: '/external-fee-test/',
   plugins: [react()],
   server: {
     host: true,
